4743170a1817d693272cb7315f29adf9a76cc2ca
Austin Morales <harrenaustint@gmail.com>
2021-03-23 13:40:34 -0400

    added proxy

--- client/src/config.js
+++ client/src/config.js
@@ -3,4 +3,4 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export const BACKEND_URL=process.env.NODE_ENV === 'development'?"http://localhost:5000/": "https://austinmorales09.github.io/cap2021/"
+export const BACKEND_URL=process.env.NODE_ENV === 'development'?"http://localhost:5000/": "mongodb://localhost/Cluster0"
