e85ba7962c8963bfcfbf17ea97444df1fb46139d
Austin Morales <harrenaustint@gmail.com>
2021-03-23 15:43:04 -0400

    added install-client to package json

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 // Import Components
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar.js";
 import Footer from "./components/Footer";
 import Home  from "./components/Home";
 import About  from "./components/About";
