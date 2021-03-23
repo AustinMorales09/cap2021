2ef663d08f8b2b60413191dfa2d5add5b5265cc5
Austin Morales <harrenaustint@gmail.com>
2021-03-23 14:32:31 -0400

    pushing new node modules

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const path = require('path')
 
 require('dotenv').config();
 
@@ -37,6 +37,9 @@ app.use('/history', historyRouter);
 
 if (process.env.NODE_ENV === "production"){
   app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client/build'))
+  })
 };
 
 
