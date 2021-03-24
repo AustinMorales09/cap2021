e21a6d6a548533ff6c1a068d01bf82b7447c8363
Austin Morales <harrenaustint@gmail.com>
2021-03-24 15:09:41 -0400

    push

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,19 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 // Import Components
-import Navbar from 'src/components/Navbar';
-import Footer from "src/components/Footer";
-import Home  from "src/components/Home";
-import About  from "src/components/About";
-import EatLocal  from "src/components/EatLocal";
-import ShopLocal  from "src/components/ShopLocal";
-import Attractions  from "src/components/Attractions";
-import CincyHistory  from "src/components/CincyHistory";
-import Contact  from "src/components/Contact";
-import CreateRestaurant from "src/components/CreateRestaurant";
-import CreateHistory from "src/components/CreateHistory";
-import CreateShop from "src/components/CreateShop";
-import CreateAttraction from "src/components/CreateAttraction";
+import Navbar from './components/Navbar';
+import Footer from "./components/Footer";
+import Home  from "./components/Home";
+import About  from "./components/About";
+import EatLocal  from "./components/EatLocal";
+import ShopLocal  from "./components/ShopLocal";
+import Attractions  from "./components/Attractions";
+import CincyHistory  from "./components/CincyHistory";
+import Contact  from "./components/Contact";
+import CreateRestaurant from "./components/CreateRestaurant";
+import CreateHistory from "./components/CreateHistory";
+import CreateShop from "./components/CreateShop";
+import CreateAttraction from "./components/CreateAttraction";
 
 
 function App() {
