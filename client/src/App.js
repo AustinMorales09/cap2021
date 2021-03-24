86d7a45ccbd9b07047c37199c7fff9a5ebfb3b96
Austin Morales <harrenaustint@gmail.com>
2021-03-24 16:58:40 -0400

    push

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -18,8 +18,8 @@ import CreateShop from './components/CreateShop';
 import CreateAttraction from './components/CreateAttraction';
 
 
-function App() {
-
+export default class App extends Component{
+  render(){
   return (
 
     <Router>
@@ -48,5 +48,5 @@ function App() {
     
   );
 }
+}
 
-export default App;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const path = require('path')
+const path = require('path');
 
 require('dotenv').config();
 
@@ -13,6 +13,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static('client/build'));
+
 const uri = process.env.ATLAS_URI;
         
 mongoose.connect( uri, { useNewUrlParser: true, useCreateIndex: true, 
